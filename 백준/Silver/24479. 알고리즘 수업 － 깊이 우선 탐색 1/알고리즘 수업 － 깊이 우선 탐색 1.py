7cd65fc6afc8c07e1b2a906fa0a1e6404f078a10
Iwmyri <80497021+WYRITL@users.noreply.github.com>
2023-04-20 03:20:28 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 604 ms, Memory: 68132 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+tmp = [0]*(n+1)
+cnt = 0
+for _ in range(m):
+  u, v = map(int, input().split())
+  graph[u].append(v)
+  graph[v].append(u)
+for i in range(n+1):
+  graph[i].sort()
+  
+def recursive_dfs(v):
+  global cnt
+  cnt += 1
+  tmp[v] = cnt
+  for w in graph[v]:
+    if tmp[w] == 0:
+      recursive_dfs(w)
+  
+recursive_dfs(r)
+for i in range(1, n+1):
+  print(tmp[i])
