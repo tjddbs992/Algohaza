fd6acd62cf4911e47fafefe0226cc2204091c5bb
Iwmyri <80497021+WYRITL@users.noreply.github.com>
2023-04-20 23:39:33 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 2128 ms, Memory: 58764 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+cnt = [0]*(n+1)
+tmp = 1
+for _ in range(m):
+  u, v = map(int, input().split())
+  graph[u].append(v)
+  graph[v].append(u)
+for i in range(n+1):
+  graph[i].sort()
+def bfs(v):
+  global tmp
+  queue = [v]
+  cnt[v] = tmp
+  while queue:
+    v = queue.pop(0)
+    for w in graph[v]:
+      if cnt[w] == 0:
+        tmp += 1
+        queue.append(w)
+        cnt[w] = tmp
+bfs(r)
+for i in range(1, n+1):
+  print(cnt[i])
